00174a60e6acf5c8bc5cf89994b0d3f13daa3765
kingswwong <467271883@qq.com>
2019-06-13 00:42:27 +0800

    Submit HomeWork

--- index.js
+++ index.js
@@ -3,14 +3,21 @@
 
 // 完成下面程序，将`name`变量中的字母全部转为为大写，输出：`'HELLO'`。
 var name = 'hello';
-// TODO write your code here...
-
+name = name.toUpperCase();
 
 // 完成下面程序，将sentence变量中的所有单词首字母变为大写，输出：'Good Afternoon, Mr Mike.'。
 var sentence = 'good afternoon, mr mike.';
-//TODO write your code here...
-
+function toFirstUpperCase() {
+    let temp = sentence.split(" ");
+    let tempSence = "";
+    for(let i = 0;i < temp.length;i++){
+        tempSence += temp[i].charAt(0).toUpperCase() + temp[i].substring(1,temp[i].length);
+        tempSence += " ";
+    }
+    return tempSence;
+}
+sentence = f();
 
 // 完成下面程序，将money变量中的金额提取出来，输出：20。
 var money = '￥20';
-//TODO write your code here...
+money = money.replace('￥','')
